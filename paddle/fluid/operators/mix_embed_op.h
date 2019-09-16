e75b48b6d0b3f24c8585caf64e9d861ca040753f
Aurelius84 <liujiezhangbupt@gmail.com>
2019-09-16 17:11:18 +0800

    fix sum_num dataType

--- paddle/fluid/operators/mix_embed_op.h
+++ paddle/fluid/operators/mix_embed_op.h
@@ -74,222 +74,223 @@ class MixEmbedKernel : public framework::OpKernel<T> {
     }
   }
 
-    void Compute(const framework::ExecutionContext &context) const override {
-      auto *ids_t = context.Input<LoDTensor>("Ids");      // int tensor
-      auto *output_t = context.Output<LoDTensor>("Out");  // float tensor
-      auto *_max_index = context.Output<Tensor>("max_index");
-      auto *_mix_char_offset = context.Output<Tensor>("mix_char_offset");
-      auto *_buffer = context.Output<Tensor>("buffer");
-      auto *table_var = context.Input<LoDTensor>("W");
-      const int mark_sqlit = context.Attr<int>("mark_idx");
-      const auto _pool_type = context.Attr<std::string>("pool_type");
-      // int64_t padding_idx = context.Attr<int64_t>("padding_idx");
+  void Compute(const framework::ExecutionContext &context) const override {
+    auto *ids_t = context.Input<LoDTensor>("Ids");      // int tensor
+    auto *output_t = context.Output<LoDTensor>("Out");  // float tensor
+    auto *_max_index = context.Output<Tensor>("max_index");
+    auto *_mix_char_offset = context.Output<Tensor>("mix_char_offset");
+    auto *_buffer = context.Output<Tensor>("buffer");
+    auto *table_var = context.Input<LoDTensor>("W");
+    const int mark_sqlit = context.Attr<int>("mark_idx");
+    const auto _pool_type = context.Attr<std::string>("pool_type");
+    // int64_t padding_idx = context.Attr<int64_t>("padding_idx");
 
-      auto ids_dims = ids_t->dims();
-      // auto _cap_l = ids_dims[0];
-      int _cap_e = ids_dims[1];
-      auto offset = ids_t->lod()[0];
-      std::vector<size_t> top_offset;
-      top_offset.resize(offset.size());
-      top_offset[0] = 0;
+    auto ids_dims = ids_t->dims();
+    // auto _cap_l = ids_dims[0];
+    int _cap_e = ids_dims[1];
+    auto offset = ids_t->lod()[0];
+    std::vector<size_t> top_offset;
+    top_offset.resize(offset.size());
+    top_offset[0] = 0;
 
-      const auto *bottom_data = ids_t->data<int64_t>();
+    const auto *bottom_data = ids_t->data<int64_t>();
 
-      int mix_char_l = ids_t->numel();
-      // if most of slots are empty,then count() may be less than offset.size()
-      if (mix_char_l < offset.size()) {
-        mix_char_l = offset.size();
+    int mix_char_l = ids_t->numel();
+    // if most of slots are empty,then count() may be less than offset.size()
+    if (mix_char_l < offset.size()) {
+      mix_char_l = offset.size();
+    }
+    _mix_char_offset->Resize(framework::make_ddim({mix_char_l}));
+    int *mix_offset = _mix_char_offset->mutable_data<int>(context.GetPlace());
+    // int64_t *ids = const_cast<int64_t *>(ids_t->data<int64_t>());
+    // int64_t ids_numel = ids_t->numel();
+
+    const auto *weights = table_var->data<T>();
+    for (int i = 0; i < top_offset.size() - 1; ++i) {
+      int w = offset[i + 1] - offset[i];
+      if (w == 0) {
+        // keep a zero embedding vector
+        top_offset[i + 1] = top_offset[i] + 1;
+        mix_offset[top_offset[i]] = 1;
+      } else {
+        top_offset[i + 1] =
+            top_offset[i] + scan_mark(bottom_data + offset[i], w, mark_sqlit,
+                                      mix_offset + top_offset[i]);
       }
-      _mix_char_offset->Resize(framework::make_ddim({mix_char_l}));
-      int *mix_offset = _mix_char_offset->mutable_data<int>(context.GetPlace());
-      // int64_t *ids = const_cast<int64_t *>(ids_t->data<int64_t>());
-      // int64_t ids_numel = ids_t->numel();
+    }
 
-      const auto *weights = table_var->data<T>();
-      for (int i = 0; i < top_offset.size() - 1; ++i) {
-        int w = offset[i + 1] - offset[i];
-        if (w == 0) {
-          // keep a zero embedding vector
-          top_offset[i + 1] = top_offset[i] + 1;
-          mix_offset[top_offset[i]] = 1;
-        } else {
-          top_offset[i + 1] =
-              top_offset[i] + scan_mark(bottom_data + offset[i], w, mark_sqlit,
-                                        mix_offset + top_offset[i]);
-        }
-      }
+    int top_l = top_offset[top_offset.size() - 1];
 
-      int top_l = top_offset[top_offset.size() - 1];
+    framework::LoD top_lod;
+    top_lod.push_back(top_offset);
+    output_t->set_lod(top_lod);
+    output_t->Resize(framework::make_ddim({top_l, _cap_e}));
 
-      framework::LoD top_lod;
-      top_lod.push_back(top_offset);
-      output_t->set_lod(top_lod);
-      output_t->Resize(framework::make_ddim({top_l, _cap_e}));
+    T *top_data = output_t->mutable_data<T>(context.GetPlace());
+    memset(top_data, 0, _cap_e * top_l * sizeof(T));
 
-      T *top_data = output_t->mutable_data<T>(context.GetPlace());
-      memset(top_data, 0, _cap_e * top_l * sizeof(T));
-
-      _max_index->Resize(framework::make_ddim({top_l, _cap_e}));
-      int *max_index = _max_index->mutable_data<int>(context.GetPlace());
-      for (int i = 0; i < offset.size() - 1; ++i) {
-        int w = offset[i + 1] - offset[i];
-        if (w > 0) {
-          int top_j = 0;
-          size_t sum_num = 0;
-          // gen sub embedding seqs
-          unsigned int top_offset_j = top_offset[i] + top_j;
+    _max_index->Resize(framework::make_ddim({top_l, _cap_e}));
+    int *max_index = _max_index->mutable_data<int>(context.GetPlace());
+    for (int i = 0; i < offset.size() - 1; ++i) {
+      int w = offset[i + 1] - offset[i];
+      if (w > 0) {
+        int top_j = 0;
+        int sum_num = 0;
+        // gen sub embedding seqs
+        unsigned int top_offset_j = top_offset[i] + top_j;
+        sum_num = mix_offset[top_offset_j];
+        _buffer->Resize(framework::make_ddim({1, sum_num, _cap_e}));
+        int64_t *sub_data = _buffer->mutable_data<int64_t>(context.GetPlace());
+        int sub_j = 0;
+        for (int j = 0; j < w; ++j) {
+          unsigned int word_idx =
+              static_cast<unsigned int>(bottom_data[offset[i] + j]);
+          if (word_idx != mark_sqlit) {
+            memcpy((void *)(sub_data + sub_j * _cap_e),
+                   (void *)(weights + word_idx * _cap_e), _cap_e * sizeof(T));
+            sub_j += 1;
+            continue;
+          }
+          pooling_ff(_pool_type, max_index, sum_num, sub_data, top_data,
+                     (const size_t)top_offset_j, _cap_e);
+          // move to next sub seqs
+          top_j++;
+          top_offset_j = top_offset[i] + top_j;
           sum_num = mix_offset[top_offset_j];
           _buffer->Resize(framework::make_ddim({1, sum_num, _cap_e}));
-          int64_t *sub_data = _buffer->mutable_data<int64_t>(context.GetPlace());
-          int sub_j = 0;
-          for (int j = 0; j < w; ++j) {
-            unsigned int word_idx =
-                static_cast<unsigned int>(bottom_data[offset[i] + j]);
-            if (word_idx != mark_sqlit) {
-              memcpy((void *)(sub_data + sub_j * _cap_e),
-                     (void *)(weights + word_idx * _cap_e), _cap_e * sizeof(T));
-              sub_j += 1;
-              continue;
-            }
-            pooling_ff(_pool_type, max_index,  sum_num, sub_data, top_data,
-                       (const size_t)top_offset_j, _cap_e);
-            // move to next sub seqs
-            top_j++;
-            top_offset_j = top_offset[i] + top_j;
-            sum_num = mix_offset[top_offset_j];
-            _buffer->Resize(framework::make_ddim({1, sum_num, _cap_e}));
-            sub_data = _buffer->mutable_data<int64_t>(context.GetPlace());
-            sub_j = 0;
-          }
-          // to handle the end group of char for each sentences
-          pooling_ff(_pool_type, max_index, sum_num, sub_data, top_data,
-                     top_offset[i] + top_j, _cap_e);
+          sub_data = _buffer->mutable_data<int64_t>(context.GetPlace());
+          sub_j = 0;
         }
+        // to handle the end group of char for each sentences
+        pooling_ff(_pool_type, max_index, sum_num, sub_data, top_data,
+                   top_offset[i] + top_j, _cap_e);
       }
     }
-  };
+  }
+};
 
-  template <typename DeviceContext, typename T>
-  class MixEmbedGradKernel : public framework::OpKernel<T> {
-   public:
-    void pooling_bp(const std::string pool_type, Tensor *_max_index,
-                    Tensor *_max_bp_buffer, const int len, T *weights,
-                    const T *top_diff, const T *sub_index, const int out_offset,
-                    const int emb_size, const float mlr) const {
-      if (pool_type == "max") {
-        _max_bp_buffer->Resize(framework::make_ddim({1, len, emb_size}));
-        T *diff = _max_bp_buffer->mutable_data<T>();
-        memset(diff, 0, len * emb_size * sizeof(T));
-        const int *max_index = _max_index->data<int>() + out_offset * emb_size;
-        for (int i = 0; i < emb_size; ++i) {
-          const int word_idx = sub_index[max_index[i]];
-          diff[max_index[i] * emb_size + i] =
-              top_diff[out_offset * emb_size + i];
-        }
-        for (int i = 0; i < len; ++i) {
-          const int word_idx = sub_index[i];
-          lego_cpu_axpby(emb_size, mlr, diff + i * emb_size, 1.0,
-                         weights + word_idx * emb_size);
-        }
-      } else {
-        if (pool_type == "default") {
-          mlr = mlr / len;
-        }
-        for (int i = 0; i < len; ++i) {
-          const int word_idx = sub_index[i];
-          lego_cpu_axpby(emb_size, mlr, top_diff + out_offset * emb_size, 1.0,
-                         weights + word_idx * emb_size);
-        }
+template <typename DeviceContext, typename T>
+class MixEmbedGradKernel : public framework::OpKernel<T> {
+ public:
+  void pooling_bp(const std::string pool_type, Tensor *_max_index,
+                  Tensor *_max_bp_buffer, const int len, T *weights,
+                  const T *top_diff, const T *sub_index, const int out_offset,
+                  const int emb_size, const float mlr) const {
+    if (pool_type == "max") {
+      _max_bp_buffer->Resize(framework::make_ddim({1, len, emb_size}));
+      T *diff = _max_bp_buffer->mutable_data<T>();
+      memset(diff, 0, len * emb_size * sizeof(T));
+      const int *max_index = _max_index->data<int>() + out_offset * emb_size;
+      for (int i = 0; i < emb_size; ++i) {
+        const int word_idx = sub_index[max_index[i]];
+        diff[max_index[i] * emb_size + i] = top_diff[out_offset * emb_size + i];
+      }
+      for (int i = 0; i < len; ++i) {
+        const int word_idx = sub_index[i];
+        lego_cpu_axpby(emb_size, mlr, diff + i * emb_size, 1.0,
+                       weights + word_idx * emb_size);
+      }
+    } else {
+      if (pool_type == "default") {
+        mlr = mlr / len;
+      }
+      for (int i = 0; i < len; ++i) {
+        const int word_idx = sub_index[i];
+        lego_cpu_axpby(emb_size, mlr, top_diff + out_offset * emb_size, 1.0,
+                       weights + word_idx * emb_size);
       }
     }
+  }
 
-    void Compute(const framework::ExecutionContext &context) const override {
-      auto *ids_t = context.Input<LoDTensor>("Ids");  // int tensor
-      auto *output_t = context.Input<LoDTensor>("Out");  // float tensor
-      auto *d_out = context.Input<LoDTensor>(framework::GradVarName("Out"));  // float tensor
-      auto *_max_index = context.Iutput<Tensor>("max_index"); 
-      auto *_mix_char_offset = context.Input<Tensor>("mix_char_offset");
-      // auto *_buffer = context.Input<Tensor>("buffer");
-      auto *table_var = context.Input<LoDTensor>("W");
-      const int mark_sqlit = context.Attr<int>("mark_idx");
-      const auto _pool_type = context.Attr<std::string>("pool_type");
-      const float _lr = context.Attr<float>("lr");
-      // const float _l1_reg = context.Attr<float>("l1_reg");
-      // const float _weight_decay = context.Attr<float>("weight_decay");
+  void Compute(const framework::ExecutionContext &context) const override {
+    auto *ids_t = context.Input<LoDTensor>("Ids");     // int tensor
+    auto *output_t = context.Input<LoDTensor>("Out");  // float tensor
+    auto *d_out = context.Input<LoDTensor>(
+        framework::GradVarName("Out"));  // float tensor
+    auto *_max_index = context.Input<Tensor>("max_index");
+    auto *_mix_char_offset = context.Input<Tensor>("mix_char_offset");
+    // auto *_buffer = context.Input<Tensor>("buffer");
+    auto *table_var = context.Input<LoDTensor>("W");
+    const int mark_sqlit = context.Attr<int>("mark_idx");
+    const auto _pool_type = context.Attr<std::string>("pool_type");
+    const float _lr = context.Attr<float>("lr");
+    // const float _l1_reg = context.Attr<float>("l1_reg");
+    // const float _weight_decay = context.Attr<float>("weight_decay");
 
-      auto _cap_l = ids_t->dims()[0];
-      auto _cap_e = ids_t->dims()[1];
+    auto _cap_l = ids_t->dims()[0];
+    auto _cap_e = ids_t->dims()[1];
 
-      // special treat when length is zero
-      if (_cap_l == 0) {
-        LOG(WARNING) << "bp a zero length input in embedding layer";
-        return;
-      }
-      // end of special treat when length is zero
+    // special treat when length is zero
+    if (_cap_l == 0) {
+      LOG(WARNING) << "bp a zero length input in embedding layer";
+      return;
+    }
+    // end of special treat when length is zero
 
-      const auto offset = ids_t->lod()[0];
-      const auto top_offset = output_t->lod()[0];
+    const auto offset = ids_t->lod()[0];
+    const auto top_offset = output_t->lod()[0];
 
-      // const auto *top_data = d_out->data<T>();
-      auto *top_diff = d_out->data<T>();
+    // const auto *top_data = d_out->data<T>();
+    auto *top_diff = d_out->data<T>();
 
-      const int64_t *bottom_data = ids_t->data<int64_t>();
-      T *weights = table_var->data<T>();
+    const int64_t *bottom_data = ids_t->data<int64_t>();
+    T *weights = (T *)table_var->data<T>();
 
-      auto mlr = -1.0 * _lr;s
-      const auto *mix_offset = _mix_char_offset->data<int>();
-      // if (_l1_reg > 1e-10) {  // L1
-      //   for (int k = 0; k < top_data->numel(); k++) {
-      //     T val = top_data[k];
-      //     int sign = (T(0) < val) - (val < T(0));
-      //     top_diff[k] += _l1_reg * sign;
-      //   }
-      // }
+    auto mlr = -1.0 * _lr;
+    int *mix_offset = _mix_char_offset->data<int>();
+    // if (_l1_reg > 1e-10) {  // L1
+    //   for (int k = 0; k < top_data->numel(); k++) {
+    //     T val = top_data[k];
+    //     int sign = (T(0) < val) - (val < T(0));
+    //     top_diff[k] += _l1_reg * sign;
+    //   }
+    // }
 
-      // if (_weight_decay > 1e-10) {  // L2
-      //   sse_axpy(top_data, top_diff, top_data->numel(), _weight_decay);
-      // }
-      Tensor *_max_bp_buffer = nullptr;
-      Tensor *_buffer = nullptr;
-      for (int i = 0; i < offset.size() - 1; ++i) {
-        int w = offset[i + 1] - offset[i];
-        if (w > 0) {
-          int top_j = 0;
-          int sum_num = 0;
-          // keep sub embedding indices
-          unsigned int top_offset_j = top_offset[i] + top_j;
-          sum_num = mix_offset[top_offset_j];
-          _buffer->Resize(framework::make_ddim({1, sum_num, 1}));
-          int64_t *sub_data = _buffer->mutable_data<int64_t>(context.GetPlace());
-          int sub_j = 0;
+    // if (_weight_decay > 1e-10) {  // L2
+    //   sse_axpy(top_data, top_diff, top_data->numel(), _weight_decay);
+    // }
+    Tensor *_max_bp_buffer = nullptr;
+    Tensor *_buffer = nullptr;
+    for (int i = 0; i < offset.size() - 1; ++i) {
+      int w = offset[i + 1] - offset[i];
+      if (w > 0) {
+        int top_j = 0;
+        int sum_num = 0;
+        // keep sub embedding indices
+        unsigned int top_offset_j = top_offset[i] + top_j;
+        sum_num = mix_offset[top_offset_j];
+        _buffer->Resize(framework::make_ddim({1, sum_num, 1}));
+        int64_t *sub_data = _buffer->mutable_data<int64_t>(context.GetPlace());
+        int sub_j = 0;
 
-          for (int j = 0; j < w; ++j) {
-            unsigned int word_idx =
-                static_cast<unsigned int>(bottom_data[offset[i] + j]);
-            if (word_idx != mark_sqlit) {
-              sub_data[sub_j] = word_idx;
-              sub_j += 1;
-              continue;
-            }
-            pooling_bp(_pool_type, _max_index, _max_bp_buffer, sum_num, weights,
-                       top_diff, sub_data, top_offset_j, _cap_e, mlr);
-            // move to next sub seqs
-            top_j++;
-            top_offset_j = top_offset[i] + top_j;
-            sum_num = mix_offset[top_offset_j];
-            _buffer->Resize(framework::make_ddim({1, sum_num, 1}));
-            sub_data = _buffer->mutable_data<int64_t>(context.GetPlace());
-            sub_j = 0;
+        for (int j = 0; j < w; ++j) {
+          unsigned int word_idx =
+              static_cast<unsigned int>(bottom_data[offset[i] + j]);
+          if (word_idx != mark_sqlit) {
+            sub_data[sub_j] = word_idx;
+            sub_j += 1;
+            continue;
           }
-          // to handle the group at end
           pooling_bp(_pool_type, _max_index, _max_bp_buffer, sum_num, weights,
                      top_diff, sub_data, top_offset_j, _cap_e, mlr);
-        } else {
-          LOG(WARNING) << "zero len sequence " << i << " / " << top_offset.size() - 1;
+          // move to next sub seqs
+          top_j++;
+          top_offset_j = top_offset[i] + top_j;
+          sum_num = mix_offset[top_offset_j];
+          _buffer->Resize(framework::make_ddim({1, sum_num, 1}));
+          sub_data = _buffer->mutable_data<int64_t>(context.GetPlace());
+          sub_j = 0;
         }
+        // to handle the group at end
+        pooling_bp(_pool_type, _max_index, _max_bp_buffer, sum_num, weights,
+                   top_diff, sub_data, top_offset_j, _cap_e, mlr);
+      } else {
+        LOG(WARNING) << "zero len sequence " << i << " / "
+                     << top_offset.size() - 1;
       }
     }
-  };
+  }
+};
 
 }  // namespace operators
 }  // namespace paddle
